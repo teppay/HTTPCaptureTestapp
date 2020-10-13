81e75abb7c78d2af302c6db41bc20d805ca0bc30
teppay <teppay.sec@gmail.com>
2020-10-14 03:11:31 +0900

    add Certificate Pinning

--- app/src/main/java/com/example/httpcapturetestapp/MainActivity.kt
+++ app/src/main/java/com/example/httpcapturetestapp/MainActivity.kt
@@ -2,26 +2,28 @@ package com.example.httpcapturetestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
+import org.w3c.dom.Text
 import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
 
-    val client: OkHttpClient
-    init {
-        client = OkHttpClient()
-    }
+    lateinit var client: OkHttpClient
+    lateinit var pinnedClient: OkHttpClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initHTTPClients()
 
         val ll = findViewById<LinearLayout>(R.id.linearlayout)
         val card1 = LayoutInflater.from(applicationContext).inflate(R.layout.mycard, ll, false)
@@ -32,7 +34,7 @@ class MainActivity : AppCompatActivity() {
             val resultText = findViewById<TextView>(R.id.resultText)
 
             findViewById<Button>(R.id.button).setOnClickListener {
-                getAndSetText(resources.getString(R.string.http_url), resultText)
+                getAndSetText(resources.getString(R.string.http_url), resultText, client)
             }
         }
         linearlayout.addView(card1)
@@ -44,13 +46,35 @@ class MainActivity : AppCompatActivity() {
             val resultText = findViewById<TextView>(R.id.resultText)
 
             findViewById<Button>(R.id.button).setOnClickListener {
-                getAndSetText(resources.getString(R.string.https_url), resultText)
+                getAndSetText(resources.getString(R.string.https_url), resultText, client)
             }
         }
         linearlayout.addView(card2)
+
+        val card3 = LayoutInflater.from(applicationContext).inflate(R.layout.mycard, ll, false).apply {
+            findViewById<TextView>(R.id.cardTitle).text = resources.getString(R.string.https_pinning_title)
+            findViewById<TextView>(R.id.cardSubTitle).text = resources.getString(R.string.https_pinning_url)
+
+            val resultText = findViewById<TextView>(R.id.resultText)
+
+            findViewById<Button>(R.id.button).setOnClickListener {
+                getAndSetText(resources.getString(R.string.https_pinning_url), resultText, pinnedClient)
+            }
+        }
+        linearlayout.addView(card3)
     }
 
-    fun getAndSetText(url: String, textView: TextView) {
+    fun initHTTPClients() {
+        this.client = OkHttpClient()
+        val pinner = CertificatePinner.Builder()
+            .add(resources.getString(R.string.https_pinning_url), resources.getString(R.string.https_pinning_hash))
+            .build()
+        this.pinnedClient = OkHttpClient.Builder()
+            .certificatePinner(pinner)
+            .build()
+    }
+
+    fun getAndSetText(url: String, textView: TextView, client: OkHttpClient) {
         var request = Request.Builder()
             .url(url)
             .get()
